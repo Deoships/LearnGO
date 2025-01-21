594e6640bad01178097d2dcad8f419420f1b58b7
Deoships <deoships@mail.ru>
2025-01-22 02:34:17 +0300

    todo finish project

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 func main() {
 	todos := Todos{}
-	todos.add("Купить хлеб")
-	todos.add("Сделать зарядку")
-	todos.toggle(0)
-	todos.print()
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
+	storage.Save(todos)
 }
